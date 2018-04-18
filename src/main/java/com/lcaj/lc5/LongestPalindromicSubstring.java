f817648dcf5a89d4a9031cd508073bbece625bc3
王德浩 <wangdehao@meituan.com>
2018-04-18 19:24:33 +0800

    lc5

---
+++ src/main/java/com/lcaj/lc5/LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+package com.lcaj.lc5;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangdehao on 18/4/11.
+ */
+public class LongestPalindromicSubstring {
+
+    /**
+     * test cases
+     */
+
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * algorithm
+     */
+
+    // submitted
+    public String longestPalindrome(String s) {
+        return "";
+    }
+
+    // good solutions: use value2Index to find complement
+    public String longestPalindromeGS(String s) {
+        return "";
+    }
+}
