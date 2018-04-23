b8479a68e9d370bacb4199d5c3a4f5b48c5e69f2
王德浩 <wangdehao@meituan.com>
2018-04-23 21:55:37 +0800

    lc13

--- src/main/java/com/lcaj/lc12/IntegerToRoman.java
+++ src/main/java/com/lcaj/lc12/IntegerToRoman.java
@@ -28,7 +28,7 @@ public class IntegerToRoman {
     }
 
     private static int[] stairs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    private static String[] romanMap = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public static void main(String[] args){
         int[] testInt = {0, 3, 4, 9, 58, 100, 1994};
@@ -36,6 +36,11 @@ public class IntegerToRoman {
             int testI = testInt[i];
             System.out.println(String.format("int=%-3d, roman=%s", testI, intToRoman(testI)));
         }
+
+        for(int i = 0; i < 199; i++){
+            System.out.println(String.format("int=%-3d, roman=%s", i, intToRoman(i)));
+        }
+
 
     }
 
@@ -47,7 +52,7 @@ public class IntegerToRoman {
             int r = num % level;
             int q = num / level;
             for(int i = q; i > 0; i--){
-                roman += romanMap[index];
+                roman += romans[index];
             }
             num = r;
 
