806c7a2a88bde253b8ca75c3e30c6b19e92429e4
王德浩 <wangdehao@meituan.com>
2018-05-02 16:55:15 +0800

    lc24

---
+++ src/main/java/com/lcaj/lc24/SwapNodesInPairs.java
@@ -0,0 +1,54 @@
+package com.lcaj.lc24;
+
+import com.lcaj.model.ListNode;
+
+import java.util.List;
+
+/**
+ * Created by wangdehao on 18/5/2.
+ */
+public class SwapNodesInPairs {
+
+    public static void main( String[] args ) {
+        int[][] m = {
+//                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6 },
+        };
+        ListNode[] lists = ListNode.getListFrom2dArray(m);
+        for(ListNode listNode: lists){
+            ListNode swaped = swapPairs(listNode);
+            ListNode.printListNode(swaped);
+        }
+
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre2 = dummy;
+        ListNode pre = head;
+        ListNode curr = pre.next;
+        while(curr != null){
+            // relink: swap √
+            pre.next = curr.next;
+            pre2.next = curr;
+            curr.next = pre;
+
+            // iteration
+            if(pre.next != null){
+                pre2 = pre;
+                curr = pre.next.next;
+                pre = pre.next;
+            }
+            else {
+                break;
+            }
+        }
+
+        return dummy.next;
+
+    }
+}
---
+++ src/main/java/com/lcaj/lc25/ReverseNodesInKGroup.java
@@ -0,0 +1,18 @@
+package com.lcaj.lc25;
+
+import com.lcaj.model.ListNode;
+
+/**
+ * Created by wangdehao on 18/5/2.
+ */
+public class ReverseNodesInKGroup {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+
+        return null;
+    }
+
+}
