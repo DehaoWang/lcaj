a581c9efb3c3fcdd4d342e8f1acb937b37dc3fef
王德浩 <wangdehao@meituan.com>
2018-05-18 15:34:40 +0800

    29-31

---
+++ src/main/java/com/lcaj/lc31/NextPermutation.java
@@ -0,0 +1,18 @@
+package com.lcaj.lc31;
+
+/**
+ * Created by wangdehao on 18/5/7.
+ */
+public class NextPermutation {
+
+
+    public static void main( String[] args ) {
+
+    }
+
+//    1,2,3 → 1,3,2
+//    3,2,1 → 1,2,3
+//    1,1,5 → 1,5,1
+    public static void nextPermutation(int[] nums){
+    }
+}
