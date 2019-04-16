0039131e2afa179da35c8e2ba664772bdf66a60b
王德浩 <wangdehao@meituan.com>
2019-04-16 19:19:40 +0800

    impls

--- src/main/java/com/lcaj/lc207/CourseSchedule.java
+++ src/main/java/com/lcaj/lc207/CourseSchedule.java
@@ -7,12 +7,23 @@ public class CourseSchedule {
     public static void main(String[] args) {
 
     }
+
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
 
         return false;
     }
 
     public static boolean canFinishBf(int numCourses, int[][] prerequisites) {
+        for (int i = 0; i < prerequisites.length; i++) {
+            // start from course i
+            if (canFinishStartingWithI(i, prerequisites)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean canFinishStartingWithI(int i, int[][] prerequisites) {
 
         return false;
     }
