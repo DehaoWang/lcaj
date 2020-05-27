89e508652efd9340569c9ed416ddd9d332f85231
wangdehao <wangdehao@meituan.com>
2020-05-27 10:44:27 +0800

    up

--- src/main/java/algorithms/faq/AucCalculation.java
+++ src/main/java/algorithms/faq/AucCalculation.java
@@ -2,12 +2,15 @@ package algorithms.faq;
 
 import algorithms.utils.MatrixUtils;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class AucCalculation {
     public static void main(String[] args) {
         double[][] predAndLabel = getPredAndLabel(10);
         MatrixUtils.printMatrix(predAndLabel);
+        System.out.println(aucStatistical(predAndLabel));
 
     }
 
@@ -41,7 +44,21 @@ public class AucCalculation {
     }
 
     public static double aucStatistical(double[][] predAndLabel) {
-
-        return 0;
+        Arrays.sort(predAndLabel, new Comparator<double[]>() {
+            @Override
+            public int compare(double[] a, double[] b) {
+                return a[0] < b[0] ? -1 : 1;
+            }
+        });
+        int satisfiedPair = 0;
+        int negativeCount = 0;
+        for (double[] d : predAndLabel) {
+            if (d[1] == 1.0) {
+                satisfiedPair += negativeCount;
+            } else {
+                negativeCount++;
+            }
+        }
+        return (double) satisfiedPair / (negativeCount * (predAndLabel.length - negativeCount));
     }
 }
--- src/main/java/algorithms/recursion/ConvertBtToDll.java
+++ src/main/java/algorithms/recursion/ConvertBtToDll.java
@@ -3,23 +3,47 @@ package algorithms.recursion;
 import datastructures.basic.linkedlist.DLLNode;
 import datastructures.basic.linkedlist.DoublyLinkedList;
 import datastructures.basic.tree.TreeDllNode;
+import datastructures.basic.tree.TreeNode;
 
 import java.util.Stack;
 
 public class ConvertBtToDll {
     public static void main(String[] args) {
-        TreeDllNode root = new TreeDllNode(4);
-        root.left = new TreeDllNode(2);
-        root.right = new TreeDllNode(6);
-        root.left.left = new TreeDllNode(1);
-        root.left.right = new TreeDllNode(3);
-        root.right.left = new TreeDllNode(5);
-        root.right.right = new TreeDllNode(7);
-//        TreeMethods.inorderTraversal(root);
+//        TreeDllNode root = new TreeDllNode(4);
+//        root.left = new TreeDllNode(2);
+//        root.right = new TreeDllNode(6);
+//        root.left.left = new TreeDllNode(1);
+//        root.left.right = new TreeDllNode(3);
+//        root.right.left = new TreeDllNode(5);
+//        root.right.right = new TreeDllNode(7);
+////        TreeMethods.inorderTraversal(root);
+//
+//        DoublyLinkedList doublyLinkedList = convertBtToDll(root);
+//        // TODO: 2020-05-02 implementation
+//        doublyLinkedList.printTD();
 
-        DoublyLinkedList doublyLinkedList = convertBtToDll(root);
-        // TODO: 2020-05-02 implementation 
-        doublyLinkedList.printTD();
+
+        TreeNode root2 = new TreeNode(4);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(6);
+        root2.left.left = new TreeNode(1);
+        root2.left.right = new TreeNode(3);
+        root2.right.left = new TreeNode(5);
+        root2.right.right = new TreeNode(7);
+
+        TreeNode[] dll = getHeadOfDllFromBT(root2);
+        TreeNode head = dll[0];
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.right;
+        }
+        System.out.println();
+
+        TreeNode tail = dll[1];
+        while (tail != null) {
+            System.out.print(tail.val + " -> ");
+            tail = tail.left;
+        }
     }
 
     public static DoublyLinkedList convertBtToDll(TreeDllNode root) {
@@ -80,5 +104,32 @@ public class ConvertBtToDll {
 
         }
         return new DoublyLinkedList(dummy.next);
+    }
+
+    public static TreeNode[] getHeadOfDllFromBT(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        return transformBtToDll(root);
+    }
+
+    public static TreeNode[] transformBtToDll(TreeNode curr) {
+        //
+        TreeNode[] ans = new TreeNode[2];
+        ans[0] = curr;
+        ans[1] = curr;
+        if (curr.left != null) {
+            TreeNode[] leftDll = transformBtToDll(curr.left);
+            curr.left = leftDll[1];
+            leftDll[1].right = curr;
+            ans[0] = leftDll[0];
+        }
+        if (curr.right != null) {
+            TreeNode[] rightDll = transformBtToDll(curr.right);
+            curr.right = rightDll[0];
+            rightDll[0].left = curr;
+            ans[1] = rightDll[1];
+        }
+        return ans;
     }
 }
